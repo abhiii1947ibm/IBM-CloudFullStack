2e388e4ee12f78b1897374bf56a5ab00a7ee8d74
Abhishek Nigam <nabhishek1947@ibm.com>
2026-07-20 14:26:41 +0530

    files added

--- Java/Java02/Main.java
+++
@@ -1,7 +0,0 @@
-package Java02;
-
-public class Main {
-    public static void main(String[] args){
-
-    }
-}
--- index.js
+++
@@ -1,4 +0,0 @@
-function Click() {
-  alert("Are you sure want to change the color of the text?");
-  document.getElementById("pLorem").style.color = "red";
-}
