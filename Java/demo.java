c8a0debd0b2d0f28b22381db0e56b1745a2dbb12
Abhishek Nigam <nabhishek1947@ibm.com>
2026-07-26 14:46:53 +0530

    file added

--- Java/demo.java
+++ Java/demo.java
@@ -10,8 +10,15 @@ class Calculator{
 }
 public class demo {
     public static void main(String[] args){
-        Calculator c = new Calculator();
-        System.out.println(c.add(2,3));
-        System.out.print(c.add(2,3, 4));
+        // Calculator c = new Calculator();
+        // System.out.println(c.add(2,3));
+        // System.out.print(c.add(2,3, 4));
+
+        // StringBuffer sb = new StringBuffer("");
+        // System.out.print(sb.capacity());  // 16  (by default)
+
+        StringBuffer sb = new StringBuffer("Abhi");
+        System.out.print(sb.capacity());  // 20  (16 + 4 character of Abhi)
+
     }
 }
